dfff0b781ae1eb08297f67e1bb0dabeda97f2dab
falbmun0906 <amfrancisco@iesrafaelalberti.es>
2025-11-27 01:17:33 +0100

    fix: enhance Hero and Landing components with responsive layout and footer integration

--- frontend/src/layouts/LayoutRoot.jsx
+++ frontend/src/layouts/LayoutRoot.jsx
@@ -1,14 +1,17 @@
-import {Outlet} from "react-router";
+import {Outlet, useLocation} from "react-router";
 import React, {Fragment} from "react";
 import {Footer} from "../components/Footer/Footer.jsx";
 import {Header} from "../components/Header/Header.jsx";
 
 function LayoutRoot() {
+    const location = useLocation()
+    const isLanding = location.pathname === '/'
+
     return (
         <Fragment>
             <Header/>
             <Outlet/>
-            <Footer/>
+            {!isLanding && <Footer/>}
         </Fragment>
     )
 }
